c4feb07be3e9c760224beea80be2578e6b5a3b45
memisgunduzz <memisgunduz@rissasoft.com>
2023-03-13 12:50:52 +0300

    Edited

--- app/Http/Controllers/OcrController.php
+++ app/Http/Controllers/OcrController.php
@@ -3,85 +3,85 @@
 namespace App\Http\Controllers;
 
 use thiagoalessio\TesseractOCR\TesseractOCR;
+use App\Http\Requests\OCRRequest;
 use Illuminate\Http\Request;
 use PDF;
 
 class OcrController extends Controller
 {
-    public function pdfToText($getFileName)
+    public function pdfToText($getFileName, $type = "PDF")
     {
+        try {
+            $pdf = new \Spatie\PdfToImage\Pdf('PDF/' . $getFileName);
+            $numberOfpages = $pdf->getNumberOfPages();
+            $result = [];
+            for ($i = 1; $i <= $numberOfpages; $i++) {
+                $filename = $i . "_" . $getFileName . ".jpg";
+                $pdf->setPage($i)->saveImage('IMG/' . $filename);
 
-        $resultText="";
-        $pdf = new \Spatie\PdfToImage\Pdf('PDF/'.$getFileName);
-        $numberOfpages=$pdf->getNumberOfPages();
 
-        for ($i=1; $i <= $numberOfpages; $i++) {
-            $filename=$i."_".$getFileName.".jpg";
-            $pdf->setPage($i)->saveImage('IMG/'.$filename);
+                $ocr = new TesseractOCR();
+                $ocr->image('IMG/' . $filename);
 
-
-            $ocr = new TesseractOCR();
-            $ocr->image('IMG/'.$filename);
-
-            $text=$ocr->run();
-            $resultText.=$ocr->run();
+                $text = $ocr->run();
+                $result[] = $ocr->run();
+            }
+            return response()->json([
+                'success' => true,
+                'type' => $type,
+                'pages' => $result
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'message'=>$th
+            ]);
         }
-        return $resultText;
     }
-    public function fileUpload(Request $req){
-
-        $req->validate([
-            'file' => 'required|mimes:png,jpg,jpeg,docx,pdf|max:2048'
-        ]);
-        if($req->file()) {
-            $text="";
-            $resultText="";
-            if($req->file('file')->extension()=="pdf"){ //PDF FILE
-                $fileName = time().'_'.$req->file->getClientOriginalName();
+    public function fileUpload(OCRRequest $req)
+    {
+        if ($req->file()) {
+            $text = "";
+            if ($req->file('file')->extension() == "pdf") { //PDF FILE
+                $fileName = time() . '_' . $req->file->getClientOriginalName();
                 $filePath = $req->file('file')->storeAs('PDF', $fileName, '');
 
-                $resultText=$this->pdfToText($fileName);
-
-                return response()->json([
-                    'data' => $resultText,
-                    'file_name' => $fileName,
-                ]);
-
+                return $this->pdfToText($fileName);
             }
-            if($req->file('file')->extension()=="docx"){ //DOCX FILE
-                $fileName = time().'_'.$req->file->getClientOriginalName();
+            if ($req->file('file')->extension() == "docx") { //DOCX FILE
+                $fileName = time() . '_' . $req->file->getClientOriginalName();
                 $filePath = $req->file('file')->storeAs('DOCX', $fileName, '');
 
                 $domPdfPath = base_path('vendor/dompdf/dompdf');
-                 \PhpOffice\PhpWord\Settings::setPdfRendererPath($domPdfPath);
-                 \PhpOffice\PhpWord\Settings::setPdfRendererName('DomPDF');
-                 $Content = \PhpOffice\PhpWord\IOFactory::load(public_path('DOCX/'.$fileName));
-                 $PDFWriter = \PhpOffice\PhpWord\IOFactory::createWriter($Content,'PDF');
-                 $PDFWriter->save(public_path('PDF/'.$fileName.'.pdf'));
+                \PhpOffice\PhpWord\Settings::setPdfRendererPath($domPdfPath);
+                \PhpOffice\PhpWord\Settings::setPdfRendererName('DomPDF');
+                $Content = \PhpOffice\PhpWord\IOFactory::load(public_path('DOCX/' . $fileName));
+                $PDFWriter = \PhpOffice\PhpWord\IOFactory::createWriter($Content, 'PDF');
+                $PDFWriter->save(public_path('PDF/' . $fileName . '.pdf'));
 
-                 $resultText=$this->pdfToText($fileName.'.pdf');
+                return $this->pdfToText($fileName . '.pdf', 'DOCX');
+            } else { //IMAGE FILES(PNG,JPEG,JPG)
+                try {
+                    $fileName = time() . '_' . $req->file->getClientOriginalName();
+                    $filePath = $req->file('file')->storeAs('IMG', $fileName, '');
 
-                 return response()->json([
-                    'data' => $resultText,
-                    'file_name' => $fileName,
-                ]);
+                    $ocr = new TesseractOCR();
+                    $ocr->image($filePath);
 
-            }else{ //IMAGE FILES(PNG,JPEG,JPG)
-                $fileName = time().'_'.$req->file->getClientOriginalName();
-                $filePath = $req->file('file')->storeAs('IMG', $fileName, '');
+                    $text = $ocr->run();
 
-                $ocr = new TesseractOCR();
-                $ocr->image($filePath);
-
-                $text=$ocr->run();
-
-
-
-                return response()->json([
-                    'data' => $text,
-                    'file_name' => $fileName,
-                ]);
+                    return response()->json([
+                        'text' => $text,
+                        'success' => true,
+                        'type' => $req->file('file')->extension(),
+                    ]);
+                } catch (\Throwable $th) {
+                    return response()->json([
+                        'success' => false,
+                        'message'=>$th
+                    ]);
+                }
             }
         }
-   }
+    }
 }
